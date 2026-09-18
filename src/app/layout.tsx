8e6bb65e4af74233b44ad03b9553fa42ac20377e
Joao Paulo <jaozindasilva007@gmail.com>
2026-09-17 22:04:37 -0300

    style: update Lifme typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { JetBrains_Mono, Manrope } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const manrope = Manrope({
+  variable: "--font-manrope",
+  subsets: ["latin"],
+});
+
+const jetBrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrains",
   subsets: ["latin"],
 });
 
@@ -15,7 +20,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={geistSans.variable}>
+    <html lang="en" className={`${manrope.variable} ${jetBrainsMono.variable}`}>
       <body>{children}</body>
     </html>
   );
