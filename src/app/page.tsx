315b5c255abad7797bc7487627a2591e7134836d
Joao Paulo <jaozindasilva007@gmail.com>
2026-09-20 00:27:31 -0300

    style: adjust page spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Hero from "@/components/Hero";
 import Kingdoms from "@/components/Kingdoms";
 import Navbar from "@/components/Navbar";
+
 import Footer from "@/components/Footer";
 import FeaturedSpecies from "@/components/FeaturedSpecies";
 import ExploreLife from "@/components/ExploreLife";
 import About from "@/components/About";
-
-
-
 
 export default function Home() {
   return (
